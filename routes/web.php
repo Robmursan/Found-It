ffe4a597fad20f7cbfecaa3a65b17132c93dfe0a
Robmursan <2123100486@soy.utj.edu.mx>
2025-02-12 21:06:26 -0600

    mejora de estilos en la vista de inicio de sesión: corrección de padding, alineación y estética

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\controladorSesion;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/* Route::get('/', function () {
+    return view('layouts.login');
+}); */
+
+Route::get("/",controladorSesion::class);//aqui se rutea el controlador , donde esta alojada la funcion 
